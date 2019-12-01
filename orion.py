6c543824be7e43c2bcca2b5a4048c1b0bb31f8e1
Shantanu <thegeek.004@gmail.com>
2019-12-01 12:54:57 +0530

    add music playing functionality

---
+++ orion.py
@@ -0,0 +1,30 @@
+import os
+import random
+import argparse
+import subprocess
+
+
+def main(cmd):
+    print('Command:', cmd)
+    
+    if cmd == 'play music':
+        music_dir_path = '/media/shan/Entertainment/Music/Anime/Naruto Music/'
+        music_file = random.choice([
+            f for f in os.listdir(music_dir_path)
+            if f.endswith('.mp3')
+        ])
+        subprocess.run(['vlc', os.path.join(music_dir_path, music_file)])
+    elif cmd.startswith('search '):
+        pass
+    elif cmd.starswith('open '):
+        pass
+    else:
+        print('Please enter a valid command.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--cmd', help='Instructions for the assistant')
+    args = parser.parse_args()
+
+    main(args.cmd.lower().strip())
