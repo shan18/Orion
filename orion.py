630e8bc387ba8415a72eb5f47ad6bd46859acfeb
Shantanu <thegeek.004@gmail.com>
2019-12-01 16:32:19 +0530

    take instructions continuously unless specified

--- orion.py
+++ orion.py
@@ -1,13 +1,12 @@
 import os
+import sys
 import random
 import argparse
 import subprocess
 import webbrowser
 
 
-def play_music():
-    # TODO: YouTube streaming option
-    music_dir_path = '/media/shan/Entertainment/Music/Anime/Naruto Music/'
+def play_music(music_dir_path):
     music_file = random.choice([
         f for f in os.listdir(music_dir_path)
         if f.endswith('.mp3')
@@ -15,35 +14,43 @@ def play_music():
     subprocess.run(['vlc', os.path.join(music_dir_path, music_file)])
 
 
-def search(cmd):
-    search_query = '+'.join(cmd.split()[1:])
+def search(instruction):
+    search_query = '+'.join(instruction.split()[1:])
     webbrowser.open('http://www.google.com/search?q=%s' % search_query)
 
 
-def open_url(cmd):
+def open_url(instruction):
     # TODO: Display error for invalid URLs
-    url = cmd.split()[1]
+    url = instruction.split()[1]
     if not url.startswith('http://'):
         url = 'http://' + url
     webbrowser.open(url)
 
 
-def main(cmd):
-    print('Command:', cmd)
-    
-    if cmd == 'play music':
-        play_music()
-    elif cmd.startswith('search '):
-        search(cmd)
-    elif cmd.startswith('open '):
-        open_url(cmd)
-    else:
-        print('Please enter a valid command.')
+def main(args):
+    instruction = input('Please enter an instruction:\n').lower().strip()
+    while True:
+        if instruction == 'play music':
+            play_music(args.music_dir)
+        elif instruction.startswith('search '):
+            search(instruction)
+        elif instruction.startswith('open '):
+            open_url(instruction)
+        elif instruction in ['exit', 'quit', 'close']:
+            print('\nBye.')
+            sys.exit(0)
+        else:
+            print('Please enter a valid instruction.')
+        instruction = input('\nPlease enter the next instruction:\n').lower().strip()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--cmd', help='Instructions for the assistant')
+    parser.add_argument(
+        '-m', '--music_dir',
+        default='/media/shan/Entertainment/Music/Anime/Naruto Music/',
+        help='Directory containing files for playing music'
+    )
     args = parser.parse_args()
 
-    main(args.cmd.lower().strip())
+    main(args)
