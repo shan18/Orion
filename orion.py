80fe86c973c7b8f1132009d93b0865fb9ddbabaa
Shantanu <thegeek.004@gmail.com>
2019-12-01 16:14:06 +0530

    improve code readability

--- orion.py
+++ orion.py
@@ -5,26 +5,38 @@ import subprocess
 import webbrowser
 
 
+def play_music():
+    # TODO: YouTube streaming option
+    music_dir_path = '/media/shan/Entertainment/Music/Anime/Naruto Music/'
+    music_file = random.choice([
+        f for f in os.listdir(music_dir_path)
+        if f.endswith('.mp3')
+    ])
+    subprocess.run(['vlc', os.path.join(music_dir_path, music_file)])
+
+
+def search(cmd):
+    search_query = '+'.join(cmd.split()[1:])
+    webbrowser.open('http://www.google.com/search?q=%s' % search_query)
+
+
+def open_url(cmd):
+    # TODO: Display error for invalid URLs
+    url = cmd.split()[1]
+    if not url.startswith('http://'):
+        url = 'http://' + url
+    webbrowser.open(url)
+
+
 def main(cmd):
     print('Command:', cmd)
     
     if cmd == 'play music':
-        # TODO: YouTube streaming option
-        music_dir_path = '/media/shan/Entertainment/Music/Anime/Naruto Music/'
-        music_file = random.choice([
-            f for f in os.listdir(music_dir_path)
-            if f.endswith('.mp3')
-        ])
-        subprocess.run(['vlc', os.path.join(music_dir_path, music_file)])
+        play_music()
     elif cmd.startswith('search '):
-        search_query = '+'.join(cmd.split()[1:])
-        webbrowser.open('http://www.google.com/search?q=%s' % search_query)
+        search(cmd)
     elif cmd.startswith('open '):
-        # TODO: Display error for invalid URLs
-        url = cmd.split()[1]
-        if not url.startswith('http://'):
-            url = 'http://' + url
-        webbrowser.open(url)
+        open_url(cmd)
     else:
         print('Please enter a valid command.')
 
